bedd380b855a269d4c9bc73b9687ccf20b81bd37
Guillaume Augustoni <guillaume.augustoni@epfl.ch>
2021-04-12 21:55:01 +0200

    Requete

--- src/Adresse.tsx
+++ src/Adresse.tsx
@@ -1,24 +1,27 @@
 import React, { useState } from "react";
 
-export function Adresse(props:any) {
-    const [name, setName] = useState("");
-  
-    const handleSubmit = (evt:any) => {
-        evt.preventDefault();
-        alert(`Submitting Adresse ${name}`)
-    }
-    return (
-      <form onSubmit={handleSubmit}>
-        <label>
-          Adresse:
-          <input
-            type="text"
-            value={name}
-            onChange={e => setName(e.target.value)}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  
+export function Adresse(props: any) {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (evt: any) => {
+    evt.preventDefault();
+    let requete = `https://api-adresse.data.gouv.fr/search/?q=${name}`
+    alert(`Requete ${requete}`);
+    fetch(requete)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Adresse:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
 }
