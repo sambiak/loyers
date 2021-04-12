e0b6d13b37863969583ce6676f5c9722692c2ce6
Guillaume Augustoni <guillaume.augustoni@epfl.ch>
2021-04-12 22:14:12 +0200

    ajout de lecture des cords

--- src/Adresse.tsx
+++ src/Adresse.tsx
@@ -1,7 +1,48 @@
 import React, { useState } from "react";
 
+
+export interface Gps {
+    type:        string;
+    version:     string;
+    features:    Feature[];
+    attribution: string;
+    licence:     string;
+    query:       string;
+    limit:       number;
+}
+
+export interface Feature {
+    type:       string;
+    geometry:   Geometry;
+    properties: Properties;
+}
+
+export interface Geometry {
+    type:        string;
+    coordinates: number[];
+}
+
+export interface Properties {
+    label:        string;
+    score:        number;
+    housenumber?: string;
+    id:           string;
+    name:         string;
+    postcode:     string;
+    citycode:     string;
+    x:            number;
+    y:            number;
+    city:         string;
+    context:      string;
+    type:         string;
+    importance:   number;
+    street?:      string;
+}
+
+
 export function Adresse(props: any) {
   const [name, setName] = useState("");
+  const [cordonnés, setCord] = useState("");
 
   const handleSubmit = (evt: any) => {
     evt.preventDefault();
@@ -9,10 +50,18 @@ export function Adresse(props: any) {
     alert(`Requete ${requete}`);
     fetch(requete)
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data:Gps) => {
+        console.log(data);
+        setCord(data.features[0].geometry.coordinates.toString());
+
+    });
   };
   return (
+      
     <form onSubmit={handleSubmit}>
+        <p>
+          Vos coordonées : {cordonnés}
+      </p>
       <label>
         Adresse:
         <input
