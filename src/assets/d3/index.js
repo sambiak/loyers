9dff40e3ed9134ed1fe6604956b8e3bb0dde8231
leohen <leohenriot13@gmail.com>
2021-08-09 15:30:13 +0200

    ff

---
+++ src/assets/d3/index.js
@@ -0,0 +1,16 @@
+"use strict";
+exports.__esModule = true;
+var d3_geo_1 = require("d3-geo");
+var d3_1 = require("d3");
+var d3 = {
+    geoEqualEarth: d3_geo_1.geoEqualEarth,
+    geoMercator: d3_1.geoMercator,
+    geoContains: d3_geo_1.geoContains,
+    geoPath: d3_geo_1.geoPath,
+    csv: d3_1.csv,
+    tsv: d3_1.tsv,
+    dsv: d3_1.dsv,
+    json: d3_1.json,
+    select: d3_1.select
+};
+exports["default"] = d3;
